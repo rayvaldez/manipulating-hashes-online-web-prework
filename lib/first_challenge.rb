04de7ec4a4fd8143d83b34d30b62d3b088e1a1fc
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-19 12:25:39 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   contacts.each do |person, data|
     data.each do |attribute, value|
-      value.delete_if { |attribute, value| value == "strawberry"}
+      data.delete_if { |attribute, value| value == "strawberry"}
     end
   end
 
